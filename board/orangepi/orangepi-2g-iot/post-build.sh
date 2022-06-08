1e34fba7415f40b061e854b94b541723c5459730
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2022-06-08 15:33:04 +0300

    Updated board/orangepi/orangepi-2g-iot/post-build.sh
    Thanks to https://github.com/eurol and https://4pda.to/forum/index.php\?showtopic\=813602\&view\=findpost\&p\=115327991

--- board/orangepi/orangepi-2g-iot/post-build.sh
+++ board/orangepi/orangepi-2g-iot/post-build.sh
@@ -8,7 +8,7 @@ BOOT_CMD=$BOARD_DIR/boot.cmd
 BOOT_CMD_H=$BINARIES_DIR/boot.scr
 
 git clone https://github.com/orangepi-xunlong/OrangePiRDA_external.git /tmp/OrangePiRDA_external
-cp /tmp/OrangePiRDA_external/bootarg/modem.bin $BINARIES_DIR
+cp /tmp/OrangePiRDA_external/chips/RDA/bootarg/modem.bin $BINARIES_DIR
 rm -fR /tmp/OrangePiRDA_external
 
 # U-Boot script
